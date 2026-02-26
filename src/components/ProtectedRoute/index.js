dbb37e38a167dc23e13d458db99d73508ff02d5b
Jaya Chandra Katta <jayachandra.katta1@gmail.com>
2026-02-26 15:41:55 +0530

    Fixed ProtectedRoute redirect loop

--- src/components/ProtectedRoute/index.js
+++ src/components/ProtectedRoute/index.js
@@ -1,10 +1,10 @@
-import {Navigate} from 'react-router-dom'
+import { Navigate } from 'react-router-dom'
 
-const ProtectedRoute = ({children}) => {
+const ProtectedRoute = ({ children }) => {
   const isLoggedIn = localStorage.getItem('login')
 
   if (!isLoggedIn) {
-    return <Navigate to="/login" />
+    return <Navigate to="/login" replace />
   }
 
   return children
