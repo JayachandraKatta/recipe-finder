89bd9de2e9b1a0af583cb6edb7c99795cf81ba1c
Jaya Chandra Katta <jayachandra.katta1@gmail.com>
2026-02-26 15:31:22 +0530

    Fixed GitHub Pages blank page issue

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 
 import Login from './components/Login'
 import Home from './components/Home'
@@ -7,37 +7,35 @@ import ProtectedRoute from './components/ProtectedRoute'
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
+    <Routes>
 
-        {/* Login Page */}
-        <Route path="/login" element={<Login />} />
+      {/* Login Page */}
+      <Route path="/login" element={<Login />} />
 
-        {/* Home Page (Protected) */}
-        <Route
-          path="/"
-          element={
-            <ProtectedRoute>
-              <Home />
-            </ProtectedRoute>
-          }
-        />
+      {/* Home Page */}
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        }
+      />
 
-        {/* Recipe Details Page (Protected) */}
-        <Route
-          path="/recipe/:id"
-          element={
-            <ProtectedRoute>
-              <RecipeDetails />
-            </ProtectedRoute>
-          }
-        />
+      {/* Recipe Details */}
+      <Route
+        path="/recipe/:id"
+        element={
+          <ProtectedRoute>
+            <RecipeDetails />
+          </ProtectedRoute>
+        }
+      />
 
-        {/* Redirect Unknown Routes */}
-        <Route path="*" element={<Navigate to="/" replace />} />
+      {/* Redirect Unknown Routes */}
+      <Route path="*" element={<Navigate to="/" replace />} />
 
-      </Routes>
-    </BrowserRouter>
+    </Routes>
   )
 }
 
